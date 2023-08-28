582ede86949e15adb128df6b959b4a21785f330b
EnvyL27 <farrel.ahmad.zaidan@gmail.com>
2023-08-28 09:39:56 +0700

    update route

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { AuthService } from '../service/auth/auth.service';
+import { Component, OnInit, Type } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private AuthService: AuthService, private router: Router) { }
+  loginForm!: FormGroup;
 
+  alert: boolean = false;
+  alertMessage: any;
+
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
+
+  login() {
+    if (this.loginForm.value.password.length < 6) {
+      this.alert = true;
+      this.alertMessage = "Password must be greater than 6 characters";
+      setTimeout(() => {
+        this.alert = false;
+        this.alertMessage = "";
+      }, 2500);
+      return;
+    }
+
+    this.AuthService.Login(this.loginForm.value).subscribe((res: any) => {
+      this.AuthService.SetToken(res.token);
+      this.router.navigateByUrl('/dashboard');
+    }, (err: any) => {
+      this.alert = true;
+      this.alertMessage = err.error.message;
+      setTimeout(() => {
+        this.alert = false;
+        this.alertMessage = "";
+      }, 2500);
+    })
+  }
 }
