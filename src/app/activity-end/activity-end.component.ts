b3e095ccc3d77d78da22f7f0bc79fa088049b168
EnvyL27 <farrel.ahmad.zaidan@gmail.com>
2024-02-13 16:10:40 +0700

    add table for schedule

--- src/app/activity-end/activity-end.component.ts
+++ src/app/activity-end/activity-end.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CountService } from '../service/master/count.service';
 import { Chart } from 'chart.js/auto';
 import { NgxSpinnerService } from 'ngx-spinner';
+import * as moment from 'moment';
 
 
 @Component({
@@ -27,13 +28,13 @@ export class ActivityEndComponent implements OnInit {
   sectionData: any = []
   sectionFiltered: any = []
   sectionShow: boolean = false
-  cardShow : boolean = false
-  imagePopUp : boolean = false
-  addPopUp : boolean = false
-  imageUrl : any
-  getEmployee : any
-  cardData : any[] = []
-  
+  cardShow: boolean = false
+  imagePopUp: boolean = false
+  addPopUp: boolean = false
+  imageUrl: any
+  getEmployee: any
+  cardData: any[] = []
+
   selectedColor: string = "#9C27B0";
   colorToAdd: string = "#EC407A";
   colorPalette: Array<any> = [
@@ -57,48 +58,44 @@ export class ActivityEndComponent implements OnInit {
     this.colorPalette.push(this.colorToAdd);
   }
 
-  logColor($event : any){
+  logColor($event: any) {
     console.log($event);
     console.log(this.colorToAdd);
-    
-    
   }
 
-  
-
-  addOpen(){
+  addOpen() {
     this.addPopUp = !this.addPopUp
   }
 
-  cancelAdd(){
+  cancelAdd() {
     this.addPopUp = false
   }
 
-  imageOpen($event : any){
+  imageOpen($event: any) {
     this.imageUrl = $event
     this.imagePopUp = !this.imagePopUp
   }
 
-  cancelPopUp(){
+  cancelPopUp() {
     this.imagePopUp = false
   }
 
-  cardOpen($event : any) {
+  cardOpen($event: any) {
     console.log($event);
-    
+
     this.cardData = []
     this.cardShow = !this.cardShow
     console.log(this.krmList);
-    
+
     this.krmList.forEach(element => {
-      if(element.id_data == $event){
+      if (element.id_data == $event) {
         this.cardData.push(element)
       }
     });
     console.log(this.cardData);
-    
+
   }
-  cardClose(){
+  cardClose() {
     this.cardShow = false
   }
   constructor(private service: CountService, private spinner: NgxSpinnerService) { }
@@ -142,7 +139,7 @@ export class ActivityEndComponent implements OnInit {
         });
 
         console.log(this.krmList);
-        
+
 
       })
 
@@ -174,7 +171,7 @@ export class ActivityEndComponent implements OnInit {
   resetFilter() {
     this.area = ''
     this.section = ''
-    this.filter  = false
+    this.filter = false
     this.krmList = []
     this.service.getKrmData().subscribe(data => {
       this.krmData = data
@@ -190,26 +187,93 @@ export class ActivityEndComponent implements OnInit {
 
   }
 
+
+  checkShift(date_shift: any, date_activity: any) {
+
+    const [startHours, startMinutes, startSeconds] = date_shift.plan_start.split(':').map(Number);
+    const [endHours, endMinutes, endSeconds] = date_shift.plan_end.split(':').map(Number);
+
+    const totalStartMinutes = (startHours * 60) + startMinutes + (startSeconds / 60);
+    const totalEndMinutes = (endHours * 60) + endMinutes + (endSeconds / 60);
+    const totalDuration = totalEndMinutes - totalStartMinutes;
+    console.log( date_shift.plan_start);
+    console.log(totalStartMinutes);
+    
+    
+    // console.log(totalDuration);
+
+    let returnVar: any;
+
+    if (date_activity[1] === date_shift.date_start) {
+
+      if (totalStartMinutes > 420 && totalStartMinutes < 900 && date_activity[0] == 'S1') {
+        returnVar = 'S1';
+      } else if (totalStartMinutes > 900 && totalStartMinutes < 1380 && date_activity[0] == 'S2') {
+        returnVar = 'S2';
+      } else if (((totalStartMinutes > 0 && totalStartMinutes < 420) || (totalStartMinutes > 1380 && totalStartMinutes < 1440))  && date_activity[0] == 'S3') {
+        returnVar = 'S3';
+      }
+      console.log(returnVar);
+    }
+
+    
+    
+
+    return returnVar;
+  }
+
+
+  shift = ['S1', 'S2', 'S3']
+  shiftArray: any[] = []
+  getDateRange = (firstDate: any, lastDate: any) => {
+
+    if (moment(firstDate, 'YYYY-MM-DD').isSame(moment(lastDate, 'YYYY-MM-DD'), 'day'))
+      return [lastDate];
+    let date = firstDate;
+    this.shiftArray = [['S1', date], ['S2', date], ['S3', date]]
+    const dates = [date];
+    do {
+      date = moment(date).add(1, 'day');
+      dates.push(date.format('YYYY-MM-DD'));
+      this.shiftArray.push(['S1', date.format('YYYY-MM-DD')], ['S2', date.format('YYYY-MM-DD')], ['S3', date.format('YYYY-MM-DD')])
+    } while (moment(date).isBefore(lastDate));
+    return dates;
+  };
+  dates: any
+
   async ngOnInit(): Promise<void> {
-    
-    
+
+    var dateStartPart: any
+    var dateStart: any
+    var dateEndPart: any
+    var dateEnd: any
+    var range: any
+
     window.scrollTo(0, 0);
     this.loaddata = new Promise(resolve => {
       this.service.getSchedule().subscribe(data => {
         console.log(data);
-        
+
         this.krmData = data
         this.krmData.forEach((element: any) => {
-          if(element.id_header == 1){
+          if (element.id_header == 1) {
             this.krmList.push(element)
           }
-         
+
         });
-        console.log(this.krmList);
+        dateStart = this.krmList[0].date_start
+        dateEnd = this.krmList[this.krmList.length - 1].date_start
+        this.dates = this.getDateRange(dateStart, dateEnd)
+        console.log(this.shiftArray);
+
+        dateStartPart = dateStart.slice(-2);
+        dateEndPart = dateEnd.slice(-2);
+        range = dateEndPart - dateStartPart
 
         this.spinner.hide()
         this.resolved = true
       }
+
       );
 
       this.service.getEmployeeData().subscribe(data => {
